c073f386bd03df829c56b37b28e4a422912ea388
Al-Abir <sheikhabir196@gmail.com>
2024-12-21 10:10:29 +0600

    Array

---
+++ Array/Roated_Sorted.java
@@ -0,0 +1,42 @@
+package Array;
+
+public class Roated_Sorted {
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Otherwise, the right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
+    }
+    public static void main(String[] args) {
+        
+        int nums[] = {4,5,6,7,0,1,2};
+        int target = 0;
+
+        System.out.print(search(nums, target));
+    }
+}
