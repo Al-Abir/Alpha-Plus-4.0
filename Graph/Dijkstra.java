fbfd8ffbeff6bbeff64f58bba0717d1a2c87bcfc
Al-Abir <sheikhabir196@gmail.com>
2025-01-26 14:06:40 +0600

    Graph

--- Graph/Dijkstra.java
+++ Graph/Dijkstra.java
@@ -6,10 +6,12 @@ import java.util.PriorityQueue;
 
 public class Dijkstra {
     public static class Edge {
+        int src;
         int des;
         int wt;
 
-        Edge(int des, int wt) {
+        Edge(int src, int des, int wt) {
+            this.src = src;
             this.des = des;
             this.wt = wt;
         }
@@ -20,26 +22,26 @@ public class Dijkstra {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(1, 2));
-        graph[0].add(new Edge(2, 4));
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(3, 7));
-        graph[1].add(new Edge(2, 1));
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
 
-        graph[2].add(new Edge(4, 3));
+        graph[2].add(new Edge(2, 4, 3));
 
-        graph[3].add(new Edge(5, 1));
+        graph[3].add(new Edge(3, 5, 1));
 
-        graph[4].add(new Edge(3, 2));
-        graph[4].add(new Edge(5, 5));
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
     }
 
     public static class Pair implements Comparable<Pair> {
-        int node;
+        int n;
         int path;
 
-        Pair(int node, int path) {
-            this.node = node;
+        Pair(int n, int path) {
+            this.n = n;
             this.path = path;
         }
 
@@ -49,48 +51,45 @@ public class Dijkstra {
         }
     }
 
-    public static void Dijkstra_algo(ArrayList<Edge>[] graph, int src) {
-        int[] dis = new int[graph.length];
+    public static void Dijkstra_algo(ArrayList<Edge>[] graph) {
+        int dis[] = new int[graph.length];
         Arrays.fill(dis, (int) 1e8);
-        dis[src] = 0;
+        dis[0] = 0; // Distance to source node is 0
 
-        boolean[] vis = new boolean[graph.length];
+        boolean vis[] = new boolean[graph.length];
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(src, 0));
+        pq.add(new Pair(0, 0)); // Source node with distance 0
 
         while (!pq.isEmpty()) {
             Pair curr = pq.remove();
-            if (!vis[curr.node]) {
-                vis[curr.node] = true;
+            if (!vis[curr.n]) {
+                vis[curr.n] = true;
 
-                for (Edge e : graph[curr.node]) {
+                for (Edge e : graph[curr.n]) {
+                    int u = e.src;
                     int v = e.des;
                     int wt = e.wt;
 
-                    if (dis[curr.node] + wt < dis[v]) {
-                        dis[v] = dis[curr.node] + wt;
+                    if (dis[u] + wt < dis[v]) {
+                        dis[v] = dis[u] + wt;
                         pq.add(new Pair(v, dis[v]));
                     }
                 }
             }
         }
 
-        // Print distances
-        for (int i = 0; i < dis.length; i++) {
-            if (dis[i] == (int) 1e8) {
-                System.out.print("INF ");
-            } else {
-                System.out.print(dis[i] + " ");
-            }
+        for (int i : dis) {
+            System.out.print((i == (int) 1e8 ? "INF" : i) + " ");
         }
     }
 
     public static void main(String[] args) {
         int v = 6;
+
+        @SuppressWarnings("unchecked")
         ArrayList<Edge>[] graph = new ArrayList[v];
         CreateGraph(graph);
 
-        // Run Dijkstra's algorithm from source node 0
-        Dijkstra_algo(graph, 0);
+        Dijkstra_algo(graph);
     }
 }
